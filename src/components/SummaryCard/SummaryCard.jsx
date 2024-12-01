f9f8376b37894039c53ffe9fc9bdc154ddd18ddb
ponnanna-ks <ponnanna.ks@logycent.com>
2024-12-01 16:41:14 +0530

    summary card styling

--- src/components/SummaryCard/SummaryCard.jsx
+++ src/components/SummaryCard/SummaryCard.jsx
@@ -1,29 +1,35 @@
 import RatingCard from '../ratingCard/ratingCard'
 import styles from './SummaryCard.module.css'
 
-export default function SummaryCard({restaurantData}) {
+export default function SummaryCard({ restaurantData }) {
     return (
-        <section className={styles.container}>
-            <div key={restaurantData._id} className={styles.cardPR}>
+        <section
+            className={styles.container}
+        >
+            <div
+                key={restaurantData._id}
+                className={styles.cardPR}
+                style={{ backgroundImage: `url(${restaurantData.bgImg})` }}
+            >
                 <div className={styles.imageContainer}>
-                    <img src={restaurantData.bgImg} alt={restaurantData.title} />
                     <div className={styles.content}>
                         <p className={styles.subtitle}>{restaurantData.tagLine}</p>
                         <h3 className={styles.title}>{restaurantData.name}</h3>
-                        <div style={{ display: "flex", flexDirection: "row"}}>
+                        <div style={{ display: "flex", flexDirection: "row" }}>
                             <div className={styles.button}>{`Minimum Order: $${restaurantData.minOrder}`}</div>
-                            <div className={styles.button}>{`Delivery In: ${parseInt(restaurantData.deliveryTime)-5}-${restaurantData.deliveryTime} Minutes`}</div>
+                            <div className={styles.button}>{`Delivery In: ${parseInt(restaurantData.deliveryTime) - 5}-${restaurantData.deliveryTime} Minutes`}</div>
                         </div>
                     </div>
-                    <div className={styles.cardImg}>
-                        <img src={restaurantData.logo} alt={restaurantData.title}/>
-                        <RatingCard rating={restaurantData.rating}/>
-                    </div>
+                </div>
+                <div className={styles.cardImg}>
+                    <img src={restaurantData.logo} alt={restaurantData.title} />
+                    <RatingCard rating={restaurantData.rating} styling={styles.ratingCard} />
                 </div>
                 <div className={styles.openInfo}>
-                    <h3>{`Open until ${restaurantData.openTill}`}</h3>
+                    {`Open until ${restaurantData.openTill}`}
                 </div>
             </div>
         </section>
+
     )
 }
--- src/components/ratingCard/ratingCard.jsx
+++ src/components/ratingCard/ratingCard.jsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { IoStarSharp, IoStarOutline, IoStarHalfSharp } from 'react-icons/io5';
 import styles from './ratingCard.module.css';
 
-function RatingCard({ rating }) {
+function RatingCard({ rating, styling }) {
     const [fullStars, setFullStars] = useState(0);
     const [halfStars, setHalfStars] = useState(0);
     const [emptyStars, setEmptyStars] = useState(0);
 
     useEffect(() => {
-        if(rating>5){
+        if (rating > 5) {
             rating = 5
         }
         const fullStars = Math.floor(rating);
@@ -20,28 +20,31 @@ function RatingCard({ rating }) {
     }, [rating])
 
     return (
-        <section className={styles.container}>
-            <h3>{rating}</h3>
+        <div className={styling && styling}>
+            <section className={styles.container}>
+                <h3>{rating}</h3>
 
-            <div>
-                {/* Render full stars */}
-                {fullStars > 0 && [...Array(fullStars)].map((_, index) => (
-                    <IoStarSharp key={`full-${index}`} color="#FC8A06" />
-                ))}
+                <div>
+                    {/* Render full stars */}
+                    {fullStars > 0 && [...Array(fullStars)].map((_, index) => (
+                        <IoStarSharp key={`full-${index}`} color="#FC8A06" />
+                    ))}
 
-                {/* Render half stars */}
-                {halfStars > 0 && [...Array(halfStars)].map((_, index) => (
-                    <IoStarHalfSharp key={`half-${index}`} color="#FC8A06" />
-                ))}
+                    {/* Render half stars */}
+                    {halfStars > 0 && [...Array(halfStars)].map((_, index) => (
+                        <IoStarHalfSharp key={`half-${index}`} color="#FC8A06" />
+                    ))}
 
-                {/* Render full stars */}
-                {emptyStars > 0 && [...Array(emptyStars)].map((_, index) => (
-                    <IoStarOutline key={`full-${index}`} color="#FC8A06" />
-                ))}
-            </div>
+                    {/* Render full stars */}
+                    {emptyStars > 0 && [...Array(emptyStars)].map((_, index) => (
+                        <IoStarOutline key={`full-${index}`} color="#FC8A06" />
+                    ))}
+                </div>
 
-            <p className={styles.reviews}>1370 reviews</p>
-        </section>
+                <p className={styles.reviews}>1370 reviews</p>
+            </section>
+
+        </div>
     );
 }
 
