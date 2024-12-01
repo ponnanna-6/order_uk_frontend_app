482b09b3607029196e04eb0b8f95ca7fa36b9b46
ponnanna-ks <ponnanna.ks@logycent.com>
2024-12-01 21:51:57 +0530

    order summary styling

--- src/components/orderSummary/orderSummary.jsx
+++ src/components/orderSummary/orderSummary.jsx
@@ -1,37 +1,62 @@
 import styles from './orderSummary.module.css';
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { GrFormNext } from "react-icons/gr";
-const OrderSummary = ({cartData, onClickDelivery, onClickPayment}) => {
+import { FaLocationDot } from "react-icons/fa6";
+const OrderSummary = ({ cartData, onClickDelivery, onClickPayment }) => {
     return (
         <div className={styles.container}>
             <div className={styles.orderDetails}>
-                <h2><span><IoMdArrowRoundBack /></span>Order Summary</h2>
-                {cartData.map((item, index) => (
-                    <div key={index} className={styles.itemContainer}>
-                        <div className={styles.itemImage}>
-                            <img src={item.foodInfo.image} alt={item.foodInfo.name} />
+                <h2><span><IoMdArrowRoundBack onClick={() => window.history.back()} /></span>Your Order Details</h2>
+                <div className={styles.divider}>
+                    {cartData.map((item, index) => (
+                        <div key={index} className={styles.itemContainer} style={index === cartData.length - 1 ? { borderBottom: "none" } : null}>
+                            <div className={styles.itemImage}>
+                                <img src={item.foodInfo.image} alt={item.foodInfo.name} />
+                            </div>
+                            <div className={styles.itemDetails}>
+                                <h3 className={styles.itemName}>{item.foodInfo.name}</h3>
+                                <span className={styles.itemQuantity}>{item.quantity}x item</span>
+                            </div>
+
+                            <p className={styles.itemPrice}>${item.foodInfo.price * item.quantity}</p>
                         </div>
-                        <div className={styles.itemDetails}>
-                            <h3 className={styles.itemName}>{item.foodInfo.name}</h3>
-                            <p className={styles.itemPrice}>₹{item.foodInfo.price}</p>
-                            <span className={styles.itemQuantity}>Quantity: {item.quantity}</span>
-                        </div>
+                    ))}
+                    <div className={styles.orderNotes}>
+                        <p>Notes</p>
+                        <textarea placeholder="Add order notes" />
                     </div>
-                ))}
-                <div className={styles.orderNotes}>
-                    <textarea placeholder="Add order notes" />
                 </div>
             </div>
             <div className={styles.orderSummary}>
-                <div className={styles.deliveryAddress}>
-                    <h3>Delivery Address</h3>
-                    <p>45, Green Street, Sector 12</p>
-                    <GrFormNext onClick={onClickDelivery}/>
+                <div className={styles.deliveryAddress} onClick={onClickDelivery}>
+                    <div style={{ display: "flex", flexDirection: "row" }}>
+                        <div className={styles.locationIcon}>
+                            <FaLocationDot color='#FC8A06' />
+                        </div>
+                        <div className={styles.addressDetails}>
+                            <h3>Delivery Address</h3>
+                            <p>45, Green Street, Sector 12 sajdshjdhakd sadnsadkjsadkjhsakjdhsakjhdkjsa</p>
+                        </div>
+                    </div>
+                    <GrFormNext color='#FC8A06' style={{ fontSize: "2.5rem" }} />
                 </div>
+                <div className={styles.dividerLine}></div>
+
                 <div className={styles.orderInfo}>
-                    <p>Items: ₹230</p>
-                    <p>Sales Tax: ₹10</p>
-                    <p className={styles.total}>Subtotal (3 items): ₹240</p>
+                    <div className={styles.orderSummaryItems}>
+                        <p>Items</p>
+                        <p>$230</p>
+                    </div>
+                    <div className={styles.orderSummaryItems}>
+                        <p>Sales Tax</p>
+                        <p>10</p>
+                    </div>
+                    <div className={styles.dividerLine}></div>
+                    
+                    <div className={styles.orderSummaryItems}>
+                        <h4>{`Subtotal (${cartData.length} items)`}</h4>
+                        <h4>$400</h4>
+                    </div>
                 </div>
                 <button className={styles.paymentButton} onClick={onClickPayment}>Choose Payment Method</button>
             </div>
